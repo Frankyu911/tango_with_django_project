49941f0aad94783bd75b96c66f03877959392dd0
Frankyu911 <yefanyu911@gmail.com>
2020-02-09 17:19:29 +0000

    Chapter 9.6 Registration

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -93,6 +93,10 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -100,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
